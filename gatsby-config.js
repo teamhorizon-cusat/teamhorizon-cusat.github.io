1d05ccdc71bbe149f3290ce32719a9e385ed581c
Anito Anto <anitoperamangalam@gmail.com>
2021-05-07 00:05:57 +0530

    Added deployment with gh-pages gatsby package, team images debug-2

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "Team Horizon",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/assets/icon.png`,
+      },
+    },
+  ],
 };
