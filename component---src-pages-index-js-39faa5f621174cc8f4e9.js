5818a7683995b5248dfad77335fb3640552a913c
Anito Anto <anitoperamangalam@gmail.com>
2021-05-07 00:08:40 +0530

    Updates

---
+++ component---src-pages-index-js-39faa5f621174cc8f4e9.js
@@ -0,0 +1,2 @@
+(self.webpackChunkteam_horizon=self.webpackChunkteam_horizon||[]).push([[678],{7704:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return r}});var u=t(7294);function r(){return u.createElement("div",null,"WEBSITE UNDER DEVELOPMENT")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-39faa5f621174cc8f4e9.js.map
