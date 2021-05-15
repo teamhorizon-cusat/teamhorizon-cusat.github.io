9df30d0788e8808408a533b9b650dedff7f0c069
Anito Anto <anitoperamangalam@gmail.com>
2021-05-15 08:10:31 +0530

    Fixed window is undefined on build

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,40 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./style.css";
-import scrollTo from 'gatsby-plugin-smoothscroll';
+import scrollTo from "gatsby-plugin-smoothscroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-
 export default function Navbar() {
   const [colorChange, setColorchange] = useState(false);
-  const changeNavColor = ()=>{
-    if(window.scrollY>80){
+  const changeNavColor = () => {
+    if (window.scrollY > 80) {
       setColorchange(true);
-    }else{
+    } else {
       setColorchange(false);
     }
   };
-  window.addEventListener('scroll',changeNavColor);
+  const isBrowser = typeof window !== "undefined";
+  if (isBrowser) {
+    window.addEventListener("scroll", changeNavColor);
+  }
   return (
-      <nav className={colorChange ? 'navscroll': 'navbar'}>
-        <h1>Horizon</h1>
-        <div>
-          <button onClick={() => scrollTo('#about')}>About</button>
-          <button onClick={() => scrollTo('#team')}>Team</button>
-          <button onClick={() => scrollTo('#contact-us')}>Contact Us</button>
-          </div>
-          <div className="hamburger">
-          <div className="dropbtn"><FontAwesomeIcon icon={faBars}/></div>
-            <ul className="dropDown">
-            <button onClick={() => scrollTo('#about')}>About</button>  
-            <button onClick={() => scrollTo('#team')}>Team</button>  
-            <button onClick={() => scrollTo('#contact-us')}>Contact Us</button>  
-            </ul>
-          </div>
-      </nav>
+    <nav className={colorChange ? "navscroll" : "navbar"}>
+      <h1>Horizon</h1>
+      <div>
+        <button onClick={() => scrollTo("#about")}>About</button>
+        <button onClick={() => scrollTo("#team")}>Team</button>
+        <button onClick={() => scrollTo("#contact-us")}>Contact Us</button>
+      </div>
+      <div className="hamburger">
+        <div className="dropbtn">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <ul className="dropDown">
+          <button onClick={() => scrollTo("#about")}>About</button>
+          <button onClick={() => scrollTo("#team")}>Team</button>
+          <button onClick={() => scrollTo("#contact-us")}>Contact Us</button>
+        </ul>
+      </div>
+    </nav>
   );
 }
