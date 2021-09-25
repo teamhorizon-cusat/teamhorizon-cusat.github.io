3d47c7d8c998ce363abc8608dfba7e9fa8d94524
agentmike007 <manujasan23@gmail.com>
2021-09-25 15:53:03 +0530

    added all styles

--- src/components/Photos/Modal.js
+++ src/components/Photos/Modal.js
@@ -1,24 +1,21 @@
-import {Modal} from "react-bootstrap"
+import { Modal } from "react-bootstrap"
 import React from "react"
 
-
 function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className="modal__content"
+    >
+      <Modal.Header className="modal__header"></Modal.Header>
+      <Modal.Body className="modal__body">
+        <img src={props.img} alt="images of the rover" />
+      </Modal.Body>
+    </Modal>
+  )
+}
 
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header >
-        </Modal.Header>
-        <Modal.Body>
-          <img src={props.img} alt="images of the rover"/>
-        </Modal.Body>
-      </Modal>
-    );
-  }
-
-
-export default MyVerticallyCenteredModal;
+export default MyVerticallyCenteredModal
